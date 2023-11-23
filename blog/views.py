11dfac2651a27eeaa3a2b4f499dbf8282fcf09dd
Hugo Ceriani <100979006+GoldFingerTold@users.noreply.github.com>
2023-11-23 17:46:51 -0300

    Front de Blog

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from .forms import PostCreateForm
+from .models import Post
 
-# Create your views here.
+class PostCreateView(View):
+    # Puedes añadir algún comentario o una línea de código aquí
+    pass
+
 class BlogListView(View):
     def get(self, request, *args, **kwargs):
-        context={
-
-        }
+        context = {}
         return render(request, 'blog_list.html', context)
-    
 
 class BlogCreateView(View):
     def get(self, request, *args, **kwargs):
-        context={
-
+        form = PostCreateForm()
+        context = {
+            'form': form
         }
         return render(request, 'blog_create.html', context)
     
-    
     def post(self, request, *args, **kwargs):
-        context={
+        if request.method == "POST":
+            form = PostCreateForm(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data.get('title')
+                content = form.cleaned_data.get('content')
+                p, created = Post.objects.get_or_create(title=title, content=content)
+                p.save()
+                return redirect('blog:home')
 
-        }
+        context = {}
         return render(request, 'blog_create.html', context)
